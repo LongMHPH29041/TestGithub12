d7034b17084d6b6843393f087ba0e86997fbaae2
long2 <longmhph29041@fpt.edu.vn>
2023-03-16 22:08:34 +0700

    TestGithub13

---
+++ app/src/main/java/h2clt/fpt/testgithub12/PH29041.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.testgithub12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PH29041 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
