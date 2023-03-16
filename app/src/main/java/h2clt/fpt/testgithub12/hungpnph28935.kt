6db5fdf331293ecc856b2cbfc6181d0cc0882db3
DELL <hungpnph28935@fpt.edu.vn>
2023-03-16 22:32:26 +0700

    tesAgain1

--- app/src/main/java/h2clt/fpt/testgithub12/hungpnph28935.kt
+++ app/src/main/java/h2clt/fpt/testgithub12/hungpnph28935.kt
@@ -7,5 +7,7 @@ class hungpnph28935 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hungpnph28935)
+        //tesst lan 2 xem su thay doi cua cac nhanh voi nhau ok dm
+
     }
 }
