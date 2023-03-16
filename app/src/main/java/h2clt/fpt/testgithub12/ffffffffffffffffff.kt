a0f3d5c85357e569992ac5b35f9c4b58b487cc22
Cuong <cuongncph28957@fpt.edu.vn>
2023-03-16 22:17:53 +0700

    PH28957

---
+++ app/src/main/java/h2clt/fpt/testgithub12/ffffffffffffffffff.kt
@@ -0,0 +1,11 @@
+package h2clt.fpt.testgithub12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ffffffffffffffffff : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ffffffffffffffffff)
+    }
+}
